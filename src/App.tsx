3b5a5f0ba40285d4d5df1d1d788afefabca7df68
Diego Rocha <diego.dpr@hotmail.com>
2022-12-08 14:36:13 -0300

    transactions search form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-import { Transactions } from "./pages/transactions";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
